f8de64f3ea640d9ef8c7205cdb92c4c45abc5dc0
Tanya Zvereva <darkmarketgirl3@yandex.ru>
2021-03-13 13:29:02 +0300

    commit

---
+++ src/js/__tests__/game.test.js
@@ -0,0 +1,31 @@
+import {
+  sortHeroes
+} from "../game"
+test("should sort heroes", function () {
+  const result = sortHeroes([{
+      name: 'мечник',
+      health: 10
+    },
+    {
+      name: 'маг',
+      health: 100
+    },
+    {
+      name: 'лучник',
+      health: 80
+    },
+  ]);
+  expect(result).toEqual([{
+      name: 'маг',
+      health: 100
+    },
+    {
+      name: 'лучник',
+      health: 80
+    },
+    {
+      name: 'мечник',
+      health: 10
+    },
+  ]);
+})
